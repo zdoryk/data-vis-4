022b5b8a192c3bf0a874d76b80ce056c2308313b
zdoryk <nice.zdorik@gmail.com>
2022-05-30 11:11:57 +0200

    Links generator was added
    Script that can define links between nodes.

---
+++ parser/qwe.py
@@ -0,0 +1,53 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+page = requests.get("https://www.imdb.com/chart/top/?ref_=nv_mv_250")
+
+test = {}
+temp = []
+
+if page.status_code == 200:
+    bs = BeautifulSoup(page.content, 'lxml')
+    # print(bs)
+    temp = [x.text.strip().replace('\n', '') for x in bs.find("ul", {"class": "quicklinks"}).findAll('a')]
+    # print(temp)
+
+for i in temp:
+    test[i] = []
+
+print(test)
+
+file = open('json_nodes.json', 'r', encoding='UTF-16')
+
+q = json.load(file)
+
+sources = {}
+
+for i in q['nodes'][:10]:
+    for k, v in test.items():
+        if k in i['genres']:
+            v.append(i['title'])
+
+all_sources = []
+
+for k, v in test.items():
+    # print(v)
+    for source in v:
+        for target in v:
+            if source != target:
+                all_sources.append(
+                    {
+                        'source': source,
+                        'target': target,
+                        'value': 1
+                    }
+                )
+
+print(all_sources)
+q['links'] = all_sources
+
+print(q)
+
+with open("json_nodes_with_links.json", "w", encoding="UTF-16") as file:
+    json.dump(q, file, indent=4)
